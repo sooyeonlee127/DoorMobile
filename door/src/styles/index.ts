1dfb59b6f4b68c635dc9de6c1aa720286ac3ef51
sooyeon <sooyeonlee127@eco.co.kr>
2023-11-23 18:07:33 +0900

    feat: 반응형 작업

--- door/src/styles/index.ts
+++ door/src/styles/index.ts
@@ -1,16 +1,8 @@
-import { css, useTheme } from '@emotion/react';
+import { css } from '@emotion/react';
 import theme from './theme';
 
 const fontSize = 16;
 const maxWidth = 430;
-
-const xxs = 12
-const xs = 13
-const sm = 14
-const base = 16
-const md = 18
-const lg = 20
-
 
 export const GlobalStyle = css({
   '.card-main': {
@@ -108,94 +100,17 @@ const TextSizeCheck = (text: number, width: number) => {
     },
   })
 }
-export const BaseTextResponsive = TextSizeCheck(base, 460)
 
-export const SmTextResponsive = TextSizeCheck(sm, 460)
-
-export const MdTextResponsive = TextSizeCheck(md, 460)
-
-export const LgTextResponsive = TextSizeCheck(lg, 460)
+const xxs = 12
+const xs = 13
+const sm = 14
+const base = 16
+const md = 18
+const lg = 20
 
 export const XxsTextResponsive = TextSizeCheck(xxs, 460)
-
-
-// export const SmTextResponsive = css({
-//     // // 폰트 반응형
-//   '@media (max-width: 420px)': {
-//     fontSize: `${ fontSize * 420 / maxWidth}px`,
-//   },
-//   '@media (max-width: 410px)': {
-//     fontSize: `${ fontSize * 410 / maxWidth}px`,
-//   },
-//   '@media (max-width: 400px)': {
-//     fontSize: `${ fontSize * 400 / maxWidth}px`,
-//   },
-//   '@media (max-width: 390px)': {
-//     fontSize: `${ fontSize * 390 / maxWidth}px`,
-//   },
-//   '@media (max-width: 380px)': {
-//     fontSize: `${ fontSize * 380 / maxWidth}px`,
-//   },
-//   '@media (max-width: 370px)': {
-//     fontSize: `${ fontSize * 370 / maxWidth}px`,
-//   },
-//   '@media (max-width: 360px)': {
-//     fontSize: `${ fontSize * 360 / maxWidth}px`,
-//   },
-//   '@media (max-width: 350px)': {
-//     fontSize: `${ fontSize * 350 / maxWidth}px`,
-//   },
-//   '@media (max-width: 340px)': {
-//     fontSize: `${ fontSize * 340 / maxWidth}px`,
-//   },
-//   '@media (max-width: 330px)': {
-//     fontSize: `${ fontSize * 330 / maxWidth}px`,
-//   },
-//   '@media (max-width: 310px)': {
-//     fontSize: `${ fontSize * 310 / maxWidth}px`,
-//   },
-//   '@media (max-width: 300px)': {
-//     fontSize: `${ fontSize * 300 / maxWidth}px`,
-//   },
-// })
-
-
-// export const MdTextResponsive = css({
-//     // // 폰트 반응형
-//   '@media (max-width: 420px)': {
-//     fontSize: `${ md * 420 / maxWidth}px`,
-//   },
-//   '@media (max-width: 410px)': {
-//     fontSize: `${ md * 410 / maxWidth}px`,
-//   },
-//   '@media (max-width: 400px)': {
-//     fontSize: `${ md * 400 / maxWidth}px`,
-//   },
-//   '@media (max-width: 390px)': {
-//     fontSize: `${ md * 390 / maxWidth}px`,
-//   },
-//   '@media (max-width: 380px)': {
-//     fontSize: `${ md * 380 / maxWidth}px`,
-//   },
-//   '@media (max-width: 370px)': {
-//     fontSize: `${ md * 370 / maxWidth}px`,
-//   },
-//   '@media (max-width: 360px)': {
-//     fontSize: `${ md * 360 / maxWidth}px`,
-//   },
-//   '@media (max-width: 350px)': {
-//     fontSize: `${ md * 350 / maxWidth}px`,
-//   },
-//   '@media (max-width: 340px)': {
-//     fontSize: `${ md * 340 / maxWidth}px`,
-//   },
-//   '@media (max-width: 330px)': {
-//     fontSize: `${ md * 330 / maxWidth}px`,
-//   },
-//   '@media (max-width: 310px)': {
-//     fontSize: `${ md * 310 / maxWidth}px`,
-//   },
-//   '@media (max-width: 300px)': {
-//     fontSize: `${ md * 300 / maxWidth}px`,
-//   },
-// })
+export const XsTextResponsive = TextSizeCheck(xs, 460)
+export const BaseTextResponsive = TextSizeCheck(base, 460)
+export const SmTextResponsive = TextSizeCheck(sm, 460)
+export const MdTextResponsive = TextSizeCheck(md, 460)
+export const LgTextResponsive = TextSizeCheck(lg, 460)
