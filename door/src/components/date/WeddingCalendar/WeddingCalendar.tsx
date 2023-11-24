07bcf5fd1ddd47262d495e31a54d455fa1e2aa0e
sooyeon <sooyeonlee127@eco.co.kr>
2023-11-24 17:37:20 +0900

    feat: 캘린더 페이지 생성

---
+++ door/src/components/date/WeddingCalendar/WeddingCalendar.tsx
@@ -0,0 +1,9 @@
+import { CalendarContainer } from "./WeddingCalendar.style";
+
+const WeddingCalendar = () => {
+
+    return (
+        <CalendarContainer></CalendarContainer>
+    )
+}
+export default WeddingCalendar;
